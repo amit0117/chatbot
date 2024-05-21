b017f0cbdd1a47258b13fd573547a1f29d2acc9c
Amit Kumar <amitdnagar4@gmail.com>
2024-05-21 16:46:32 +0530

    removed deployed link

--- frontend/chatbot/app/utils/socket.ts
+++ frontend/chatbot/app/utils/socket.ts
@@ -1,8 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-// const socket: Socket = io("http://localhost:3001");
-const socket: Socket = io(
-  "https://backend-3x5rdhdo7-amits-projects-4fec813e.vercel.app/"
-);
+const socket: Socket = io("http://localhost:3001");
+
 
 export default socket;
